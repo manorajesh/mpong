d0f9e1a385b9534297af2f616ea7883036bd2bfa
manorajesh <mano@raje.sh>
2022-06-14 16:17:37 -0700

    Bug fixes and play testing

--- pong.py
+++ pong.py
@@ -1,6 +1,6 @@
 import readchar as rc
 from time import sleep
-from random import randint
+from random import randint, sample
 from threading import Thread
 import sys
 
@@ -8,12 +8,14 @@ def init(height, width):
     global paddle_length
     array = []
     for i in range(height):
-        array.append([1 if i < paddle_length else 0]) # left paddle
+        array.append([0])
         for j in range(width):
             array[i].append(0)
 
-    for c in range(paddle_length): # right paddle
-        array[c][width-1] = 1
+    center_paddle = height//2-paddle_length//2  # center the paddle in y-axis
+    for c in range(paddle_length):
+        array[c+center_paddle][0] = 1           # left paddle
+        array[c+center_paddle][width-1] = 1     # right paddle
 
     array[randint((height+4-height), height-4)][randint((width+4-width), width-4)] = 3 # ball    
 
@@ -87,15 +89,17 @@ def update_ball(height, width, ball_wait=1):
                     display[i+direction_y][j+direction_x] = 3
                     return
     
-    restart() # ball out of bounds
+    restart(True) # ball out of bounds
 
-def draw(width, height, symbol):
+def draw(width, height):
     print("\033c")
     global display
     for i in range(width):
         for j in range(height):
-            if display[i][j] != 0:
-                print(symbol, end='')
+            if display[i][j] == 1:
+                print("┃", end="")
+            elif display[i][j] == 3:
+                print("●", end="")
             else:
                 print(' ', end='')
         print("\r")
@@ -108,25 +112,31 @@ def get_input():
 
 def print_scores(width):
     global score
-    print(("Score: " + str(score)).center(width), end="\r")
+    print(("Misses: " + str(score)).center(width), end="\r")
 
-def restart():
+def restart(close=False):
     global score
     global display
+    global direction_x
+    global direction_y
 
-    score += 1
+    direction_x = sample([-1, 1], 1)[0] # randomize direction
+    direction_y = sample([-1, 1], 1)[0]
+
+    if not close: # ball might might side of paddle
+        score += 1
     display = init(resolution_x, resolution_y)
     
 ####################### Main #######################
 
-resolution_x = 15
-resolution_y = 50
+resolution_x = 25
+resolution_y = 75
 paddle_length = resolution_y//10
 display = init(resolution_x, resolution_y)
 key = ""
 thread_flag = True
-direction_x = -1 # -1 = left, 1 = right
-direction_y = -1 # -1 = down, 1 = up
+direction_x = sample([-1, 1], 1)[0] # -1 = left, 1 = right
+direction_y = sample([-1, 1], 1)[0] # -1 = down, 1 = up
 score = 0
 counter = 0
 
@@ -142,14 +152,14 @@ def main():
     p = Thread(target=get_input, args=())
 
     p.start()
-    while (key != 'q'):
-        draw(resolution_x, resolution_y, '*')
+    while (key != 'q'): # quit
+        draw(resolution_x, resolution_y)
         update_paddle(resolution_x, resolution_y, key)
         key = ""
         update_ball(resolution_x, resolution_y, ball_wait=1)
         print_scores(resolution_y)
         counter += 1
-        sleep(0.1)
+        sleep(0.06) # change to increase tick rate
 
     thread_flag = False
     p.join()
